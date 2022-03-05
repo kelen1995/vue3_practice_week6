c86b1e18a99e0251eeea4f2720b7f88ce48b4b2f
kyle <sdfcvbjkl@gmail.com>
2022-03-05 18:16:38 +0800

    feat: 新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,35 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    children: [
+      {
+        path: 'products',
+        name: 'Products',
+        component: () => import('../views/ProductsView.vue'),
+      },
+      {
+        path: 'product/:id',
+        name: 'Product',
+        component: () => import('../views/ProductView.vue'),
+      },
+      {
+        path: 'cart',
+        name: 'Cart',
+        component: () => import('../views/CartView.vue'),
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/LoginView.vue'),
   },
+  {
+    path: '/admin',
+    name: 'Backend',
+    component: () => import('../views/BackendView.vue'),
+  },
+
 ];
 
 const router = createRouter({
